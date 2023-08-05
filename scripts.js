a027a4b88d3f285d853694d7fa704826442fee0c
Tamas Oravecz <oravecztamas@gmail.com>
2023-08-05 10:47:36 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,52 @@
+/*
+let data = {
+    photo: "images/1400-hp-pontiac-firebird-trans-am.jpg",
+    title: "Pontiac Firebird",
+    description: "The Pontiac Firebird is an American automobile that was built and produced by Pontiac from the 1967 to 2002 model years."
+  }
+  ;
+  
+  $("#photo").attr("src", data.photo);
+  $("#photo-title").append(data.title);
+  $("#photo-description").append(data.description);
+ */ 
+
+
+  let data = [
+    {
+    photo: "images/1400-hp-pontiac-firebird-trans-am.jpg",
+    title: "Pontiac Firebird",
+    description: "The Pontiac Firebird is an American automobile that was built and produced by Pontiac from the 1967 to 2002 model years."
+    },
+    {
+    photo: "images/1970-plymouth-hemi-cuda_3341.jpg",
+    title: "Plymouth Barracuda",
+    description: "The Plymouth Barracuda is a two-door pony car that was manufactured by Chrysler Corporation from 1964 through 1974 model years."
+    },
+    {
+    photo: "images/dodge-challenger-ta-front-three-quarter.jpg",
+    title: "Dodge Challenger",
+    description: "The Dodge Challenger is the name of three different generations of automobiles (two of those being pony cars) produced by American automobile manufacturer Dodge."
+    },
+    {
+    photo: "images/fb88c644.png",
+    title: "Chevrolet Camaro SS",
+    description: "The Chevrolet Camaro is a mid-size American automobile manufactured by Chevrolet, classified as a pony car. It first went on sale on September 29, 1966, for the 1967 model year and was designed to compete with the Ford Mustang."
+    },
+    {
+    photo: "images/Ford-Mustang-Mach-1-4-1024x614.jpg",
+    title: "Ford Mustang Mach 1",
+    description: "The Ford Mustang Mach 1 is a performance-oriented option package of the Ford Mustang muscle car, originally introduced in August 1968 for the 1969 model year. It was available until 1978, returned briefly in 2003, 2004, and most recently in 2021."
+    },
+    {
+    photo: "images/Kevins_RT_NZV8-58.jpg",
+    title: "Dodge Charger",
+    description: "The Dodge Charger is a model of automobile marketed by Dodge in various forms over seven generations since 1966. The first Charger was a show car in 1964. A 1965 Charger II concept car resembled the 1966 production version."
+    },
+    {
+    photo: "images/this-duramax-powered-1970-chevy-chevelle-is-an-environmentalist-s-worst-nightmare-216673_1.jpg",
+    title: "Chevrolet Chevelle SS",
+    description: "The Chevrolet Chevelle is a mid-sized automobile that was produced by Chevrolet in three generations for the 1964 through 1978 model years. Part of the General Motors (GM) A-body platform, the Chevelle was one of Chevrolet's most successful nameplates."
+    }
+  ]
+
